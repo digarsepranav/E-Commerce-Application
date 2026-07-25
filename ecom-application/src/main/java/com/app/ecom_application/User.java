a3fe03f4fbd775e2d66863180bcffdb2c0be7f3a
digarsepranav <digarsepranav@gmail.com>
2026-07-25 22:18:08 +0530

    Day 2 - JPA/H2/Entity/ID

--- ecom-application/src/main/java/com/app/ecom_application/User.java
+++ ecom-application/src/main/java/com/app/ecom_application/User.java
@@ -1,11 +1,33 @@
 package com.app.ecom_application;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+//@AllArgsConstructor
+@Entity(name = "user_table")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String lastName;
+
+//    public User(long id, String firstName, String lastName) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+//
+//    public User() {
+//    }
 }
 // this annotation creates getters and setters by itself
+// Id you shouldnt created but App should create it
+// To instantiate JPA entity default constructor is mandatory
