f81e31b1895beb625d22244d41fec26e7bada643
digarsepranav <digarsepranav@gmail.com>
2026-09-05 19:06:06 +0530

    Remove project2 from repository

--- todo/todo/src/main/java/com/app/todo/repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.app.todo.repository;
-
-import com.app.todo.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@org.springframework.stereotype.Repository
-public interface Repository extends JpaRepository<Task, Integer> {
-}
